fb24ad2902046132ec36dfaf493739e10bde7d71
Santiago Gómez Solarte <santiagogomezsolarte@gmail.com>
2024-04-09 14:55:46 -0500

    Modal Ventana Guralp

---
+++ cliente_next/src/app/components/Map/MenuCtx.js
@@ -0,0 +1,21 @@
+export default function MenuContext({ Mposition, setMPosition }) {
+    return (
+      <ul
+        style={{ left: `${Mposition.x}px`, top: `${Mposition.y}px` }}
+        className={`absolute bg-white p-1 box-content border border-slate-700 shadow-lg list-none w-max rounded-xl z-50`}
+      >
+        <li className="cursor-pointer select-none p-2 rounded-md">Editar</li>
+        <li className="cursor-pointer select-none p-2 rounded-md">Eliminar</li>
+        <li className="cursor-pointer select-none p-2 rounded-md">
+          Habilitar/Deshabilitar
+        </li>
+        <li className="cursor-pointer select-none p-2 rounded-md">
+          Agregar estación
+        </li>
+        <li className="cursor-pointer select-none p-2 rounded-md">
+          Agregar sensor
+        </li>
+      </ul>
+    );
+  }
+  
