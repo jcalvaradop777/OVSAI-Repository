c39589c93bdd2f1514e6a67b2c81ae0444f30acb
Santiago Gómez Solarte <santiagogomezsolarte@gmail.com>
2024-04-20 21:04:46 -0500

    Se agrego variables de entorno de la base de datos

--- cliente_next/src/config/env.js
+++ cliente_next/src/config/env.js
@@ -1,3 +1,6 @@
 export const ENV = {
-    API_KEY: process.env.API_KEY
-}
+  API_KEY: "blsofM73XhY4O2Ko7yXr",
+  URL_MONGOOSE:
+    "mongodb+srv://santiagogomezsolarte:Lenuxinc2024@inicial.1mdwnyk.mongodb.net/?retryWrites=true&w=majority&appName=Inicial",
+    URLBASE: "http://localhost:3000/"
+};
