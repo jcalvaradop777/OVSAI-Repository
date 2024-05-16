aa1f91a14ae4ad25cd002e1c9f0864db038b3e2d
Santiago Gómez Solarte <santiagogomezsolarte@gmail.com>
2024-05-16 13:59:23 -0500

    Agregar archivos .next

--- cliente_next/.next/server/server-reference-manifest.js
+++ cliente_next/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"+VrkpO5p/ANuRSVLKJOgfUbZBwlq66TkWZ3S7j53ZE0=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gJzCJxhaUDS6eLZxX3F94fMaHZO1/yVLHZlnU/E72wo=\"\n}"
--- cliente_next/.next/server/webpack-runtime.js
+++ cliente_next/.next/server/webpack-runtime.js
@@ -30,7 +30,7 @@
 /******/ 		// Execute the module function
 /******/ 		var threw = true;
 /******/ 		try {
-/******/ 			__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 			__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
 /******/ 			threw = false;
 /******/ 		} finally {
 /******/ 			if(threw) delete __webpack_module_cache__[moduleId];
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,11 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-<<<<<<< HEAD
-/******/ 		__webpack_require__.h = () => ("b69eedc44f83f660")
-=======
-/******/ 		__webpack_require__.h = () => ("4e88bde57370794e")
->>>>>>> mainTrazasBD
+/******/ 		__webpack_require__.h = () => ("cf8fa536b5891629")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
