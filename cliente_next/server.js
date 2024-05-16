68954d3ead9d45debc2f3f8c2bc45d283d955e4a
Santiago Gómez Solarte <santiagogomezsolarte@gmail.com>
2024-05-16 14:00:14 -0500

    Agregar sockets y chat

---
+++ cliente_next/server.js
@@ -0,0 +1,60 @@
+const { createServer } = require("node:http");
+const { parse } = require("url");
+const next = require("next");
+const { Server } = require("socket.io");
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 3000;
+
+const app = next({ dev, hostname, port });
+const handler = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const httpServer = createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true);
+      const { pathname, query } = parsedUrl;
+
+      if (pathname === "/a") {
+        await app.render(req, res, "/a", query);
+      } else if (pathname === "/b") {
+        await app.render(req, res, "/b", query);
+      } else {
+        await handler(req, res, parsedUrl);
+      }
+    } catch (err) {
+      console.error("Error occurred handling", req.url, err);
+      res.statusCode = 500;
+      res.end("internal server error");
+    }
+  })
+    .once("error", (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+
+  const io = new Server(httpServer);
+
+  // Save messages
+
+  const messages = [];
+
+  io.on("connection", (socket) => {
+    console.log(`a user connected ${socket.id}`);
+
+    socket.on("new-message", (data) => {
+      const { id, message, date } = data;
+      messages.push({
+        id: id,
+        message: message,
+        date: date
+      });   
+            
+      io.emit("update-messages", messages);
+    });
+  });
+});
